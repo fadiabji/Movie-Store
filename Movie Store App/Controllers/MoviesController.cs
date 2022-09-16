d68a01dc5f3d3590c0ab27fb6c3821ade656025c
fadiabji <fadi.abji@htomail.com>
2022-09-16 16:28:38 +0200

    Revert "create and display the movies and a page called Gallery to view the whole movies in database"
    This reverts commit aa1b87de473e2e04c4462b12988c5140b713c3c6.

--- Movie Store App/Controllers/MoviesController.cs
+++ Movie Store App/Controllers/MoviesController.cs
@@ -27,13 +27,6 @@ namespace Movie_Store_App.Controllers
                           Problem("Entity set 'ApplicationDbContext.Movies'  is null.");
         }
 
-
-        public async Task<IActionResult> Gallery()
-        {
-            return View(await _context.Movies.ToListAsync());
-                        
-        }
-
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -78,10 +71,11 @@ namespace Movie_Store_App.Controllers
 
                 movie.Image = filename;
             }
-           _context.Add(movie);
-           await _context.SaveChangesAsync();
-           return RedirectToAction(nameof(Index));
-
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+           
+            
         }
 
         // GET: Movies/Edit/5
@@ -129,15 +123,28 @@ namespace Movie_Store_App.Controllers
                 return NotFound();
             }
 
-         
-            _context.Update(movie);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-            
-           
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
         }
-
-
 
         // GET: Movies/Delete/5
         public async Task<IActionResult> Delete(int? id)
